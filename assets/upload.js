c096bc09949ffc9cd96922995f8b626f48a2526f
mitsukiraikiri <mitsukiraikiri@gmail.com>
2026-01-04 07:49:35 +0800

    update

--- assets/upload.js
+++ assets/upload.js
@@ -59,11 +59,46 @@ $(document).ready(function() {
         }
     });
     
+    // Fungsi untuk preview gambar saat file dipilih (khusus untuk tombol utama)
+    $(document).on('change', '.upload-box-main input[type="file"]', function() {
+        const file = this.files[0];
+        const uploadBox = $(this).closest('.upload-box-main');
+        const img = uploadBox.find('img');
+        
+        if (file && file.type.startsWith('image/')) {
+            const reader = new FileReader();
+            
+            reader.onload = function(e) {
+                img.attr('src', e.target.result);
+                img.show();
+                uploadBox.addClass('has-image');
+                uploadBox.find('.upload-icon, .upload-text').hide();
+            };
+            
+            reader.readAsDataURL(file);
+        }
+    });
+    
     // Fungsi untuk hapus gambar
     $(document).on('click', '.delete-btn', function(e) {
         e.preventDefault();
         e.stopPropagation();
         const uploadBox = $(this).closest('.upload-box');
+        const img = uploadBox.find('img');
+        const fileInput = uploadBox.find('input[type="file"]');
+        
+        img.attr('src', '');
+        img.hide();
+        uploadBox.removeClass('has-image');
+        uploadBox.find('.upload-icon, .upload-text').show();
+        fileInput.val('');
+    });
+    
+    // Fungsi untuk hapus gambar (khusus untuk tombol utama)
+    $(document).on('click', '.upload-box-main .delete-btn', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const uploadBox = $(this).closest('.upload-box-main');
         const img = uploadBox.find('img');
         const fileInput = uploadBox.find('input[type="file"]');
         
